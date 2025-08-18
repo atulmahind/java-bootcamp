fe24e72dc580494e339e865c47db16f1adf83766
Atul Mahind <atul.mahind@netcentric.biz>
2025-08-18 11:28:37 +0100

    Add lecture notes for Java 9

---
+++ src/main/java/in/mahind/bootcamp/java9/J9Features.java
@@ -0,0 +1,30 @@
+package in.mahind.bootcamp.java9;
+
+public class J9Features {
+
+    /*
+
+    Energy Efficiency Across Programming Languages
+
+    https://greenlab.di.uminho.pt/wp-content/uploads/2017/10/sleFinal.pdf
+
+     */
+
+    public static void main(String[] args) {
+
+        // 1. modules
+            // https://www.youtube.com/watch?v=22OW5t_Mbnk
+        // 2. JShell
+        // 3. private methods in interface
+            // reduce code duplication
+            // improve code encapsulation by hiding implementation details
+        // 4. try with resource enhancement
+            // the resource can be declared outside the try-with-resource block
+        // 5. collection factory methods
+            // static methods to create unmodifiable instances of collections
+            // List.of()
+            // Set.of()
+            // Map.of()
+
+    }
+}
